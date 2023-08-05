fcb6000ae055aa00aff9b6d7eab12fc72c83c919
Zahra <azzahradindas@gmail.com>
2023-08-05 17:43:30 +0700

    [update] load icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import '../assets/styles/satoshi.css'
 
 import { Providers } from './providers'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 export const metadata = {
   title: 'Structuring NextJS',
   description: 'Structuring NextJS'
