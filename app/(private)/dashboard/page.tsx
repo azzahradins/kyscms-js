8697decaa7d6cd1ca41a49a40b9aa38b7aa821ef
Zahra <azzahradindas@gmail.com>
2023-08-29 20:22:26 +0700

    [update] dashboard finished

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -10,83 +10,87 @@ ChartJS.register(
   LinearScale, Tooltip, Legend
 )
 
-export default function Dashboard(): JSX.Element {
+export default function Dashboard (): JSX.Element {
   return (
     <main>
       <div className='grid grid-cols-2 lg:grid-cols-3 gap-3 mb-3'>
         <Card>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <Card.Header>
             Monthly Visitor
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
+          </Card.Header>
+          <Card.Body>
             Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-          </p>
+          </Card.Body>
         </Card>
+
         <Card>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <Card.Header>
             Monthly Upload
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
+          </Card.Header>
+          <Card.Body>
             Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-          </p>
+          </Card.Body>
         </Card>
-        <Card>
-          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+
+        <Card className='hidden lg:block'>
+          <Card.Header>
             Server Status
-          </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-          </p>
+          </Card.Header>
+          <Card.Body>
+            This or that whatever
+          </Card.Body>
         </Card>
       </div>
 
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-3'>
         <Card>
-          <div className="mb-2 flex items-center justify-between">
-            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+          <Card.Header>
+            <h5 className="text-2xl font-bold leading-none text-gray-900 dark:text-white">
               Latest Upload
             </h5>
             <a className="text-sm font-medium text-cyan-600 hover:underline dark:text-cyan-500"
               href="#"> View all </a>
-          </div>
-          <div className="flow-root">
+          </Card.Header>
+          <Card.Body>
             <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-              <li className="py-1 sm:py-2">
-                <div className="flex items-center space-x-4">
-                  <div className="shrink-0">
-                    <Image
-                      alt="Neil image"
-                      className="rounded-full"
-                      height="32"
-                      src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?t=st=1692966165~exp=1692966765~hmac=be5e5f0a2501813ffc717b22325436fe19eb8f7c28a042ec3012ed7aba438d07"
-                      width="32"
-                    />
+                <li className="py-1 sm:py-2">
+                  <div className="flex items-center space-x-4">
+                    <div className="shrink-0">
+                      <Image
+                        alt="Neil image"
+                        className="rounded-full"
+                        height="32"
+                        src="https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?t=st=1692966165~exp=1692966765~hmac=be5e5f0a2501813ffc717b22325436fe19eb8f7c28a042ec3012ed7aba438d07"
+                        width="32"
+                      />
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
+                        #Title (#Subtitle)
+                      </p>
+                      <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                        Uploaded by: #Username
+                      </p>
+                    </div>
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                      #Status
+                    </div>
                   </div>
-                  <div className="min-w-0 flex-1">
-                    <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
-                      #Title (#Subtitle)
-                    </p>
-                    <p className="truncate text-sm text-gray-500 dark:text-gray-400">
-                      Uploaded by: #Username
-                    </p>
-                  </div>
-                  <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    #Status
-                  </div>
-                </div>
-              </li>
+                </li>
             </ul>
-          </div>
+          </Card.Body>
         </Card>
+
         <Card>
-          <div className="mb-2 flex items-center justify-between">
-            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-              Visit Performance Report
-            </h5>
-          </div>
-          <div className="flow-root h-36">
-            <Line data={LineDataOptions.data} className='relative' options={LineDataOptions.options} />
-          </div>
+          <Card.Header>
+            Visit Performance Report
+          </Card.Header>
+          <Card.Body className='max-h-52'>
+            This is summary for loadtime and performance issue
+            <div>
+              <Line data={LineDataOptions.data} className='relative' options={LineDataOptions.options} />
+            </div>
+          </Card.Body>
         </Card>
       </div>
     </main>
--- components/Page/Card.tsx
+++ components/Page/Card.tsx
@@ -1,6 +1,39 @@
-function Card({ children }: {children: React.ReactNode}): React.ReactElement {
+interface Props {
+  children: React.ReactNode
+  title?: string
+  className?: string
+}
+
+function Card ({
+  children,
+  title,
+  className = ''
+}: Props): React.ReactElement {
   return (
-    <div className="w-full p-6 bg-light-3 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div className={`${className} w-full p-6 bg-light-3 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700`}>
+      { children }
+    </div>
+  )
+}
+
+Card.Header = function CardHeader ({ children, ...props }: Props): React.ReactElement {
+  let component = children
+  if (typeof children === 'string') {
+    component = (
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      { children }
+    </h5>)
+  }
+  return (
+    <div className="mb-2 flex items-center justify-between">
+      { component }
+    </div>
+  )
+}
+
+Card.Body = function CardBody ({ children, ...props }: Props): React.ReactElement {
+  return (
+    <div className="flow-root font-normal text-gray-700 dark:text-gray-400">
       { children }
     </div>
   )
