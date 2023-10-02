f2abfe26c9ecb429a209962da4b6ebed9ced39bd
Zahra <azzahradindas@gmail.com>
2023-10-02 11:55:04 +0700

    update: adjust preview image

--- app/(private)/manga/detail/[slug]/(forms)/chapters/add/components/PreviewImage.tsx
+++ app/(private)/manga/detail/[slug]/(forms)/chapters/add/components/PreviewImage.tsx
@@ -9,21 +9,20 @@ interface IPreviewImage {
 export const PreviewImage = ({
   itemList
 }: IPreviewImage): React.ReactElement => {
-  const [position, setPosition] = useState(itemList?.length > 1 ? itemList[0].id : 0)
-
   return (
     <main className='w-3/4'>
       <section className='sticky top-4 bg-dark-2 p-4 rounded-lg'>
         <nav className='flex flex-row items-center justify-between mb-4'>
-          <FaArrowLeft/>
           <p>Manga Post Preview</p>
-          <FaArrowRight/>
         </nav>
-        <article className='grid grid-cols-4'>
+        <article className='grid grid-cols-4 gap-2'>
           {itemList?.map((image, index) => (
-            <div key={image.id} className=''>
-              <span className='text-sm font-semibold text-center mb-2'> Page {index + 1}</span>
-              <img src={URL.createObjectURL(image.file)} className='mb-4 max-h-65 overflow-y-auto'/>
+            <div key={image.id} className='overflow-hidden'>
+              <div className=''>
+                <p className='text-sm font-semibold text-center'>Page {index + 1}</p>
+                <p className='ml-2 font-light'>({image.file.name})</p>
+              </div>
+              <img src={URL.createObjectURL(image.file)} className='h-48 w-full object-cover overflow-hidden'/>
             </div>
           ))}
         </article>
