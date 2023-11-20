2e2a2f2feefb506226821e0acfd6d90701295d72
Zahra <azzahradindas@gmail.com>
2023-11-20 16:41:30 +0700

    (KY-CMSA-2307) Template user access

---
+++ app/(private)/access/page.tsx
@@ -0,0 +1,7 @@
+import { Title } from '@/components/Navigation/Title'
+
+export default function UserAccess (): React.ReactElement {
+  return <main className='wrapper'>
+    <Title value='User Management' className='mx-5 mt-4'/>
+  </main>
+}
